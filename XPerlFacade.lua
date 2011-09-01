2861007a6d3f30f51ba91739469f7488d6f13379
Andrew Scott <ascott18@gmail.com>
2011-09-01 05:02:04 +0000

    Fix frame level bugs
    git-svn-id: https://repos.curseforge.com/wow/xperlfacade/trunk@12 9338ad19-7a50-45c1-a0e9-df685a79a0fe

--- XPerlFacade.lua
+++ XPerlFacade.lua
@@ -10,84 +10,94 @@ local addonName = ...
 local _G, strfind, type, hooksecurefunc = 
 	  _G, strfind, type, hooksecurefunc
 	  
-local db, group
+local db, group, queueUpdate
 
 local noop = function() return end
 
-local function SkinCallback(arg, SkinID, Gloss, Backdrop, Group, Button, Colors)
-	if db then
-		db.S = SkinID
-		db.G = Gloss
-		db.B = Backdrop
-		db.C = Colors
-	end
-end
 
 local function DoStuff(self, event, addon)
-	if addon ~= addonName then return end
-	XPerlFacade = XPerlFacade or {}
-	db = XPerlFacade;
-	group = Stub:Group("XPerl")
-	group:Skin(db.S,db.G,db.B,db.C);
-	for i=1,40 do
-		local name = "XPerl_PlayerbuffFrameAuraButton"..i
-		local frame = _G[name]
-		if not frame then
-			break 
-		else
-			group:AddButton(frame,{
-				Icon		= _G[name.."icon"],
-				Cooldown 	= _G[name.."cooldown"],
-				Count 		= _G[name.."count"],
-				Border 		= _G[name.."border"],
-			})
+	if event == "ADDON_LOADED" and addon == addonName then
+		XPerlFacade = XPerlFacade or {}
+		db = XPerlFacade;
+		group = Stub:Group("XPerl")
+		group:Skin(db.S,db.G,db.B,db.C);
+		for i=1,40 do
+			local name = "XPerl_PlayerbuffFrameAuraButton"..i
+			local frame = _G[name]
+			if not frame then
+				break 
+			else
+				group:AddButton(frame,{
+					Icon		= _G[name.."icon"],
+					Cooldown 	= _G[name.."cooldown"],
+					Count 		= _G[name.."count"],
+					Border 		= _G[name.."border"],
+				})
+			end
 		end
-	end
-	for i=1,40 do
-		local name = "XPerl_PlayerdebuffFrameAuraButton"..i
-		local frame = _G[name]
-		if not frame then
-			break 
-		else
-			group:AddButton(frame,{
-				Icon		= _G[name.."icon"],
-				Cooldown 	= _G[name.."cooldown"],
-				Count 		= _G[name.."count"],
-				Border 		= _G[name.."border"],
-			})
+		for i=1,40 do
+			local name = "XPerl_PlayerdebuffFrameAuraButton"..i
+			local frame = _G[name]
+			if not frame then
+				break 
+			else
+				group:AddButton(frame,{
+					Icon		= _G[name.."icon"],
+					Cooldown 	= _G[name.."cooldown"],
+					Count 		= _G[name.."count"],
+					Border 		= _G[name.."border"],
+				})
+			end
 		end
-	end
-	for i=1,3 do
-		local name = "XPerl_PlayerbuffFrameTempEnchant"..i
-		local frame = _G[name]
-		if not frame then
-			break 
-		else
-			group:AddButton(frame,{
-				Icon		= _G[name.."icon"],
-				Cooldown 	= _G[name.."cooldown"],
-				Count 		= _G[name.."count"],
-				Border 		= _G[name.."border"],
-			})
+		for i=1,3 do
+			local name = "XPerl_PlayerbuffFrameTempEnchant"..i
+			local frame = _G[name]
+			if not frame then
+				break 
+			else
+				group:AddButton(frame,{
+					Icon		= _G[name.."icon"],
+					Cooldown 	= _G[name.."cooldown"],
+					Count 		= _G[name.."count"],
+					Border 		= _G[name.."border"],
+				})
+			end
 		end
+		if not LMB then
+			Stub:RegisterSkinCallback("XPerl", function(arg, SkinID, Gloss, Backdrop, Group, Button, Colors)
+				if db then
+					db.S = SkinID
+					db.G = Gloss
+					db.B = Backdrop
+					db.C = Colors
+				end
+			end)
+		end
+		f:UnregisterEvent(event)
 	end
-	Stub:RegisterSkinCallback("XPerl", SkinCallback)
-	f:UnregisterAllEvents()
 end
 
 
 hooksecurefunc("CreateFrame", function (_, name)	
 	if type(name) ~= "string" then return end
-	if strfind(name,"^XPerlBuff") or (strfind(name,"^XPerl_Player") and strfind(name,"FrameAuraButton")) then
+	if strfind(name,"^XPerlBuff") or strfind(name,"^XPerl_Player.*FrameAuraButton") then
 		local button = _G[name]
+		
 		local restore = button.SetFrameLevel
-		button.SetFrameLevel = noop
+		if button:CanChangeProtectedState() then
+			button.SetFrameLevel = noop
+		end
+		
 		group:AddButton(button,{
 			Icon		= _G[name.."icon"],
 			Cooldown 	= _G[name.."cooldown"],
 			Count 		= _G[name.."count"],
 			Border 		= _G[name.."border"],
-		});
+		})
+		if button:IsProtected() then
+			button.cooldown:SetFrameLevel(button:GetFrameLevel()-1)
+		end
+		
 		button.SetFrameLevel = restore
 	end
 end
